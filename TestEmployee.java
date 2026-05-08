5a8c08ff42da0a5cb9cbfe13fe62eafb2699ddbb
aneszaouche77 <aneszaouche77@gmail.com>
2026-05-08 22:45:07 +0200

    TestEmployee.java

--- TestEmployee.java
+++ TestEmployee.java
@@ -1,10 +1,11 @@
 package Anes;
 
 public class TestEmployee {
+
     public static void main(String[] args) {
 
-        Manager manager = new Manager("Karim", 5000, 1500);
+        Manager m = new Manager("Karim", 5000, 1500);
 
-        System.out.println(manager);
+        System.out.println(m);
     }
 }
