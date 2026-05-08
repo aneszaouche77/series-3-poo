9cfe18841d14614136a28a4d227a6a2251b7fbe5
aneszaouche77 <aneszaouche77@gmail.com>
2026-05-08 22:46:23 +0200

    TestStudent.java

--- TestStudent.java
+++ TestStudent.java
@@ -1,16 +1,17 @@
 package Anes;
 
 public class TestStudent {
+
     public static void main(String[] args) {
 
-        Student student = new Student("Sara", 20, 14.5);
+        Student s = new Student("Sara", 20, 14);
 
-        System.out.println(student);
+        System.out.println(s);
 
-        if (student.isPassed()) {
-            System.out.println("The student has passed.");
+        if (s.isPassed()) {
+            System.out.println("Student passed.");
         } else {
-            System.out.println("The student has failed.");
+            System.out.println("Student failed.");
         }
     }
 }
