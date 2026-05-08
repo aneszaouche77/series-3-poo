d9aa0246d602a7190accdbb934c2579d1824620c
aneszaouche77 <aneszaouche77@gmail.com>
2026-05-08 22:42:58 +0200

    Student.java

--- Student.java
+++ Student.java
@@ -1,32 +1,20 @@
 package Anes;
 
-class Student extends Person {
+public class Student extends Person {
+
     private double grade;
 
-    // Constructor
     public Student(String name, int age, double grade) {
         super(name, age);
         this.grade = grade;
     }
 
-    
-    public double getGrade() {
-        return grade;
-    }
-
-   
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
-
-    
     public boolean isPassed() {
         return grade >= 10;
     }
 
-    
+    @Override
     public String toString() {
-        return super.toString() +
-               ", Grade = " + grade;
+        return super.toString() + ", Grade: " + grade;
     }
 }
